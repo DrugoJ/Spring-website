fc28d18e41455550fa5d7fc7dd5026e81bba7bbc
DrugoJ <kirill.kuzmin2000@mail.ru>
2019-07-31 20:04:21 +0300

    Add first version of project

---
+++ website/src/main/java/com/kirillkuz/website/repos/UserRepo.java
@@ -0,0 +1,10 @@
+package com.kirillkuz.website.repos;
+
+import com.kirillkuz.website.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    User findByActivationCode(String code);
+}
---
+++ website/src/main/resources/templates/greeting.ftl
@@ -0,0 +1,6 @@
+<#import "parts/common.ftl" as c>
+
+<@c.page>
+<h5>Welcome, guest</h5>
+<div>This is a simple clone of Twiter</div>
+</@c.page>
